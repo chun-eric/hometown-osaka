31801f98dabc3579dc0d3dfafa2785ef0ca087df
Eric Chun <eric.chun88800888@gmail.com>
2024-10-22 16:20:40 +0900

    Complete Sign in modal form with click event to open close modal

--- script.js
+++ script.js
@@ -31,3 +31,44 @@ document.addEventListener("keydown", (e) => {
 /* display signin form when clicked on larger devices */
 /* exit on clicking overlay or clicking on close button top left
  */
+
+/*
+
+2. Display signup form
+3. Click on close icon or escape button, form is hidden
+
+*/
+
+// Select all signup buttons
+const signupBtns = document.querySelectorAll(
+  ".btn-secondary, .btn-secondary-side-bar"
+);
+const closeIcon = document.querySelector(".close-icon");
+const modalSignin = document.getElementById("modal-signin");
+const overlayContent = document.querySelector(".overlay-content");
+
+// Click on Sign up button
+signupBtns.forEach((btn) => {
+  // Add click event
+  btn.addEventListener("click", () => {
+    // Display signup form
+    modalSignin.classList.add("show");
+  });
+});
+
+// close modal form on clicking close icon
+closeIcon.addEventListener("click", () => {
+  modalSignin.classList.remove("show");
+});
+
+// close modal form when clicking outside the form
+modalSignin.addEventListener("click", (e) => {
+  if (!overlayContent.contains(e.target)) {
+    modalSignin.classList.remove("show");
+  }
+});
+
+// prevent clicks inside the form from closing the modal
+overlayContent.addEventListener("click", (e) => {
+  e.stopPropagation(); // prevents clicks from bubbling up to the modal
+});
